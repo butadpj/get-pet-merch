5741b1b3ca3f47d3e4773c728c0842ef662af9b1
Paul John Butad <butadpj@gmail.com>
2021-07-13 13:13:59 +0800

    Update deployment configuration for Github Pages -- disable wsEndpoint

--- src/vue-apollo.js
+++ src/vue-apollo.js
@@ -22,10 +22,8 @@ const defaultOptions = {
   httpEndpoint,
   // You can use `wss` for secure connection (recommended in production)
   // Use `null` to disable subscriptions
-  wsEndpoint:
-    process.env.VUE_APP_GRAPHQL_WS ||
-    "ws://pet-shop-platform--backend.herokuapp.com/graphql",
-  // wsEndpoint: null,
+  // wsEndpoint: process.env.VUE_APP_GRAPHQL_WS || "ws://localhost:4000/graphql",
+  wsEndpoint: null,
   // LocalStorage token
   tokenName: AUTH_TOKEN,
   // Enable Automatic Query persisting with Apollo Engine
--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,5 @@
 // vue.config.js file to be place in the root of your repository
 
 module.exports = {
-  publicPath:
-    process.env.NODE_ENV === "production"
-      ? "/pet-shop-platform-frontend/"
-      : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/get-pet-merch/" : "/",
 };
