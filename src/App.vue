3ec159993b1f03e54106411140f5723b186176ea
Paul John Butad <butadpj@gmail.com>
2021-07-13 12:18:33 +0800

    Add default product image

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,27 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
+
+<script>
+import gql from "graphql-tag";
+
+export default {
+  apollo: {
+    users: {
+      query: gql`
+        {
+          users {
+            id
+          }
+        }
+      `,
+      update: (data) => data.users,
+    },
+  },
+};
+</script>
 
 <style>
 #app {
